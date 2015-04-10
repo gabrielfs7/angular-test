50eda1a7e59a029ce008b9350a359154e0670f1f
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2015-04-09 22:50:37 -0300

    improvements

--- app.js
+++ app.js
@@ -6,36 +6,42 @@
             name: 'Product test 1',
             price: 2.95,
             description: 'Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes',
-            soldOut: true,
-            canPurchase: true
+            soldOut: false,
+            createdAt: new Date(),
+            canPurchase: true,
+            images: [
+                {
+                    full: 'http://www.iconarchive.com/download/i7999/hopstarter/soft-scraps/Coin.ico',
+                    thumb: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSooi8pjHSE4ZbhQ3IJ_-4LAEt2LaSW8LzbInQEVGea0hn8y_wj'
+                }
+            ]
         },
         {
             name: 'Product test 2',
-            price: 2.95,
+            price: 2,
             description: 'Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes',
             soldOut: false,
-            canPurchase: true
+            canPurchase: true,
+            createdAt: new Date(),
+            images: [
+                {
+                    full: 'http://www.iconarchive.com/download/i7999/hopstarter/soft-scraps/Coin.ico',
+                    thumb: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSooi8pjHSE4ZbhQ3IJ_-4LAEt2LaSW8LzbInQEVGea0hn8y_wj'
+                },
+                {
+                    full: 'http://www.iconarchive.com/download/i7999/hopstarter/soft-scraps/Coin.ico',
+                    thumb: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSooi8pjHSE4ZbhQ3IJ_-4LAEt2LaSW8LzbInQEVGea0hn8y_wj'
+                }
+            ]
         },
         {
             name: 'Product test 3',
             price: 2.95,
             description: 'Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes',
-            soldOut: true,
-            canPurchase: true
-        },
-        {
-            name: 'Product test 4',
-            price: 2.95,
-            description: 'Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes',
             soldOut: false,
-            canPurchase: false
-        },
-        {
-            name: 'Product test 5',
-            price: 2.95,
-            description: 'Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes Yes no no yes no no yes',
-            soldOut: false,
-            canPurchase: true
+            createdAt: new Date(),
+            canPurchase: true,
+            images: []
         }
     ];
 

3a969cbe231a92fa418b082d53538d48038c1834
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2015-04-19 00:50:57 -0300

    fix add review data to clean and autocomplete the reviews fields

--- app.js
+++ app.js
@@ -65,6 +65,19 @@
     );
 
     app.controller(
+        'ReviewController',
+        function () {
+            this.review = {};
+
+            this.addReview = function (product)
+            {
+                product.reviews.push(this.review);
+                this.review = {};
+            }
+        }
+    );
+
+    app.controller(
         'PanelController',
         function () {
             this.tab = 1;
